a37643604412b91b5e9f2ad0fd2631a888bc1da9
A.I. <akbarirawnn@gmail.com>
2024-09-30 22:44:09 +0700

    Add files via upload

---
+++ footer.jsx
@@ -0,0 +1,34 @@
+export default function Footer() {
+  function toTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Smooth scroll effect
+    });
+  }
+
+  return (
+    <footer className="mt-14 flex items-end justify-between px-5 py-4 sm:flex text-body-4 md:text-body-3">
+      <div className="flex flex-col md:flex-row w-full items-center justify-between">
+        <div className="flex space-x-1">
+          <span>&copy;</span>
+          <span className="text-white">2024</span>
+          <span className="font-extrabold uppercase text-white">AKBAR IRAWAN</span>
+        </div>
+        <div>
+          <span className="text-lg text-white">Have fun build this site</span>
+        </div>
+        <button
+          onClick={toTop}
+          className="col-span-2 flex items-center space-x-2 w-fit group 2xl:text-body-1"
+          style={{ cursor: 'pointer' }} // Ensure the button shows a pointer on hover
+        >
+          <span
+            className="font-extrabold uppercase duration-200 transform group-hover:scale-110 group-hover:font-black transition-transform ease-in-out text-white"
+          >
+            BACK TO TOP
+          </span>
+        </button>
+      </div>
+    </footer>
+  );
+}
---
+++ role.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+// Animation Variants
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5, // Memperpanjang jeda antar elemen menjadi 0.5 detik
+      staggerDirection: 1,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { 
+    opacity: 1, 
+    y: 0, 
+    transition: { duration: 1, ease: 'easeInOut' } // Memperpanjang durasi animasi menjadi 1 detik
+  },
+};
+
+export default function Role() {
+  const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.1 });
+
+  return (
+    <section
+      id="role"
+      className="select-none flex my-20 md:my-[12%] px-4 py-10 flex-col items-center justify-center overflow-hidden"
+      aria-label="Role Section"
+      ref={ref}
+    >
+      <motion.div
+        className="flex flex-col w-full items-start space-y-2" // Jarak antar teks
+        initial="hidden"
+        animate={inView ? 'visible' : 'hidden'}
+        variants={containerVariants}
+      >
+        {/* Teks dengan ukuran besar yang rata kiri */}
+        <motion.h1
+          className="text-left text-4xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-7xl font-medium leading-tight text-white"
+          variants={itemVariants}
+        >
+          I create elevating digital
+        </motion.h1>
+        <motion.h1
+          className="text-left text-4xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-7xl font-medium leading-tight text-white"
+          variants={itemVariants}
+        >
+          experiences that inspire
+        </motion.h1>
+        <motion.h1
+          className="text-left text-4xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-7xl font-medium leading-tight text-white"
+          variants={itemVariants}
+        >
+          and connect with people
+        </motion.h1>
+        <motion.h1
+          className="text-left text-4xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-7xl font-medium leading-tight text-white"
+          variants={itemVariants}
+        >
+          through development and design.
+        </motion.h1>
+      </motion.div>
+    </section>
+  );
+}
