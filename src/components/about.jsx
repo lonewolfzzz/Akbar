788668489def8bca637e2fa4d7bcf4ad86e4b357
A.I. <akbarirawnn@gmail.com>
2024-10-02 13:48:59 +0700

    Add files via upload

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -114,8 +114,8 @@ export default function About() {
               src="/assets/profile2.webp" // Use a single image
               alt="profile"
               className="z-1 mt-10"
-              width="500"
-              height="500"
+              width="1800"
+              height="2331"
               
             />
           </motion.div>
